05098ae9c26a76b475c051ed0310e5c2da0a9a9a
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-04-15 12:13:36 +0200

    tobin: stockTransactionsadded

---
+++ Projekt/TSEIS/StockTraderProvider/Repositories/IStockTransactionRepository.cs
@@ -0,0 +1,14 @@
+﻿using StockTraderBroker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTraderBroker.Repositories
+{
+    public interface IStockTransactionRepository
+    {
+        void addStockTransaction(StockTransaction stockTransaction);
+        StockTransaction GetTransaction(int id);
+    }
+}
---
+++ Projekt/TSEIS/StockTraderProvider/Repositories/StockTransactionRepository.cs
@@ -0,0 +1,27 @@
+﻿using StockTraderBroker.Database;
+using StockTraderBroker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTraderBroker.Repositories
+{
+    public class StockTransactionRepository : IStockTransactionRepository
+    {
+        private AppDbContext _dbContext;
+        public StockTransactionRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void addStockTransaction(StockTransaction stockTransaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StockTransaction GetTransaction(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

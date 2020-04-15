b5e70be7a51b7ac145793710bc3088e578731ecd
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-04-15 12:03:12 +0200

    tobin: taxregistration added with repo and dbcontext

--- Projekt/TSEIS/TobinTaxingControl/Database/AppDbContext.cs
+++ Projekt/TSEIS/TobinTaxingControl/Database/AppDbContext.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using StockTraderBroker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TobinTaxingControl.Models;
 
 namespace TobinTaxingControl.Database
 {
-    public class AppDbContext: DbContext
-    {
+    public class AppDbContext: DbContext 
+    { 
+        public AppDbContext()
+        {
+            LoadDefaultData();
+        }
+        public DbSet<TaxRegistration> TaxRegistrations { get; set; }
 
+        public void LoadDefaultData()
+        {
+            
+        }
     }
 }
--- Projekt/TSEIS/TobinTaxingControl/Models/TaxRegistration.cs
+++ Projekt/TSEIS/TobinTaxingControl/Models/TaxRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using StockTraderBroker.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,11 @@ namespace TobinTaxingControl.Models
 {
     public class TaxRegistration
     {
-        int id;
-        StockTransaction transaction;
-        double taxAmount;
-        bool approved;
+        [Key]
+        int id { get; set; }
+        StockTransaction transaction { get; set; }
+        double taxAmount { get; set; }
+        bool approved { get; set; }
 
     }
 }

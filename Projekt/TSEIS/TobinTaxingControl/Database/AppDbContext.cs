d149ec98304fe42e34f35017e6dc05b76c655bee
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-04-15 11:39:17 +0200

    tobin: gad ikke lige til at shelve

---
+++ Projekt/TSEIS/TobinTaxingControl/Database/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TobinTaxingControl.Database
+{
+    public class AppDbContext: DbContext
+    {
+
+    }
+}

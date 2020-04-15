752f0d2ec942cbb815e84efad4ac059d572ed926
Jannik <Jannik.lading@hotmail.dk>
2020-04-15 16:55:03 +0200

    TradedShares working with docker

---
+++ Projekt/TSEIS/TradedShares/Migrations/20200415132841_firstMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradedShares.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<double>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stocks");
+        }
+    }
+}
---
+++ Projekt/TSEIS/TradedShares/Repositories/StockRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TradedShares.Database;
+using TradedShares.Models;
+
+namespace TradedShares.Repositories
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public StockRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void AddStock(Stock stock)
+        {
+            if (stock != null)
+            {
+                _dbContext.Stocks.Add(stock);
+                _dbContext.SaveChanges();
+            }
+        }
+        public void DeleteStock(int id)
+        {
+            _dbContext.Stocks.Remove(_dbContext.Stocks.Find(id));
+            _dbContext.SaveChanges();
+        }
+
+        public Stock GetById(int id)
+        {
+            if (id != 0)
+            {
+                return _dbContext.Stocks.Find(id);
+            }
+            return null;
+        }
+
+        public List<Stock> GetStocks()
+        {
+            var stocks = new List<Stock>();
+            try
+            {
+                stocks = _dbContext.Stocks.ToList();
+            }
+            catch (Exception e)
+            {
+                //Error accured
+            }
+            return stocks;
+        }
+    }
+}

bc043d509840bcc1dc7e89f22e12d2da46d49144
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-04-15 11:26:33 +0200

    tobin: models added need refernce to stockuser

---
+++ Projekt/TSEIS/TobinTaxingControl/Models/TaxRegistration.cs
@@ -0,0 +1,17 @@
+﻿using StockTraderBroker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TobinTaxingControl.Models
+{
+    public class TaxRegistration
+    {
+        int id;
+        StockTransaction transaction;
+        double taxAmount;
+        bool approved;
+
+    }
+}

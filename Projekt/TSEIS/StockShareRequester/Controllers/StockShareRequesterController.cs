17494b28b1204add56e8e26f4046aa45eb95dc5f
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-05-11 12:50:41 +0200

    stockSharerequester: controller added gettrades and request trade

---
+++ Projekt/TSEIS/StockShareRequester/Controllers/StockShareRequesterController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StockTraderBroker.Models;
+
+namespace StockShareRequester.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockShareRequesterController : ControllerBase
+    {
+        private HttpClient client = new HttpClient();
+        private string TraderBrokerApiGetSharesUri = "some Uri";
+        private string TraderBrokerApiRequestTradeUri = "some Uri";
+
+
+        [HttpGet]
+        public async Task<IEnumerable<StockTrade>> GetActiveShares()
+        {
+            HttpResponseMessage response = await client.GetAsync(TraderBrokerApiGetSharesUri);
+
+            string responseResult = await response.Content.ReadAsStringAsync();
+            if (responseResult != null)
+            {
+                List<StockTrade> trades = (List<StockTrade>)JsonConvert.DeserializeObject(responseResult);
+                return trades;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> RequestTrade(int id, [FromBody] Models.UserDto user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            string json = JsonConvert.SerializeObject(user);
+            HttpResponseMessage response = await client.PostAsync(TraderBrokerApiRequestTradeUri, new StringContent(json, Encoding.UTF8, "application/json"));
+
+            string responseResult = await response.Content.ReadAsStringAsync();
+
+            return (IActionResult)response;
+        }
+    }
+}
--- Projekt/TSEIS/StockTraderProvider/Models/StockTrade.cs
+++ Projekt/TSEIS/StockTraderProvider/Models/StockTrade.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using TradedShares.Models;
 
 namespace StockTraderBroker.Models
 {

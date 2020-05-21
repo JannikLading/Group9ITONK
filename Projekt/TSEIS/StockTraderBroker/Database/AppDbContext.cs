66ebdea5a948c8b0ba490157509b4b158ee2c721
Jannik <Jannik.lading@hotmail.dk>
2020-05-21 15:40:13 +0200

    renamed stocktraderbroker folder

--- Projekt/TSEIS/StockTraderBroker/Database/AppDbContext.cs
+++ Projekt/TSEIS/StockTraderBroker/Database/AppDbContext.cs
@@ -11,7 +11,7 @@ namespace StockTraderBroker.Database
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            LoadDefaultData();
+            //LoadDefaultData();
         }
         public DbSet<StockTrade> StockTrades { get; set; }
 

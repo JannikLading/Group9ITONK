c0e3b7800c2dd6f103a655821e0775fc820f8e05
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-05-21 11:08:24 +0200

    UsersDto created

---
+++ Projekt/TSEIS/Users/Models/UsersDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Users.Models
+{
+    public class UsersDto
+    {
+        User Buyer { get; set; }
+        User Seller { get; set; }
+    }
+}

94f3682d3baf8deda25fe82273ae61c856ce4a9d
Jannik <Jannik.lading@hotmail.dk>
2020-05-24 13:41:59 +0200

    updated script with example

--- Projekt/TSEIS/dockerps.ps1
+++ Projekt/TSEIS/dockerps.ps1
@@ -3,3 +3,6 @@ param(  [Parameter(Mandatory)][string]$name,
 
 docker build -t $name -f ".\$project\DockerFile" . 
 docker push $name
+
+#Example
+#C:\IKT\6. semester\ITONK\Group9ITONK\Projekt\TSEIS>.\dockerps.ps1 -name "lostreidiotos/tradedshares" -project "TradedShares"
